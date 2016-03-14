62e05697d0937599180f1080ac276b35cd575c0f
jedzill4 <raulonco@gmail.com>
2016-03-14 17:47:43 -0300

    eliminacion de archivo de Carla en master

--- readwav.py
+++
@@ -1,6 +0,0 @@
-''' Read a wav file and convert it to an array (sample)
- Saves also the sample rate (int)'''
-import scipy.io.wavfile as wav
-filename_wav = "call1.wav" #Insert the wav filename
-(rate,sample) = wav.read(filename_wav) 
-
