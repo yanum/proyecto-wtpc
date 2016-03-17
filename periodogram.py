8c0b925ded5b3d148358711972bcb244f448b6f2
Luis María Pizarro <lmpizarro@gmail.com>
2016-03-17 14:45:39 -0300

    periodogram finish

--- periodogram.py
+++ periodogram.py
@@ -1,42 +1,58 @@
-import scipy.io.wavfile as wav
 import matplotlib.pyplot as plt
 
 from scipy import signal
 
-file_name = "audioFiles/albanella/call1.wav" 
-file_name2 = "audioFiles/albanella/call2.wav" 
-file_name3 = "audioFiles/averlaPiccola/call1.wav" 
-
-(rate,sample) = wav.read(file_name)
-(rate2,sample2) = wav.read(file_name2)
-(rate3,sample3) = wav.read(file_name3)
 
 class Periodogram(object):
+    '''
+    A class to calc periodograms of a signal
+    '''
+    def calc(self, samples, rate): 
+        '''
+        calc:
+        parameters: samples the data array
+        rate: the sample rate
+        '''
+        self.f, self.Pxx_den = signal.periodogram(samples, rate)
 
-     def calc(samples, rate): 
-         self.f, self.Pxx_den = signal.periodogram(samples, rate)
+
+def verificar():
+    import scipy.io.wavfile as wav
+    file_name = "audioFiles/albanella/call1.wav" 
+    file_name2 = "audioFiles/albanella/call2.wav" 
+    file_name3 = "audioFiles/averlaPiccola/call1.wav" 
+
+    (rate,sample) = wav.read(file_name)
+    (rate2,sample2) = wav.read(file_name2)
+    (rate3,sample3) = wav.read(file_name3)
 
 
-per = Periodogram(object):
+    per = Periodogram()
 
-plt.plot(f, Pxx_den)
-plt.xlabel('frequency [Hz]')
-plt.ylabel('PSD [V**2/Hz]')
-plt.show()
+    per.calc( sample, rate)
 
-f, Pxx_den = signal.periodogram(sample2, rate2)
+    plt.plot(per.f, per.Pxx_den)
+    plt.xlabel('frequency [Hz]')
+    plt.ylabel('PSD [V**2/Hz]')
+    plt.show()
 
-plt.plot(f, Pxx_den)
-plt.xlabel('frequency [Hz]')
-plt.ylabel('PSD [V**2/Hz]')
-plt.show()
+    per.calc(sample2, rate2)
 
-f, Pxx_den = signal.periodogram(sample3, rate3)
+    plt.plot(per.f, per.Pxx_den)
+    plt.xlabel('frequency [Hz]')
+    plt.ylabel('PSD [V**2/Hz]')
+    plt.show()
 
-plt.plot(f, Pxx_den)
-plt.xlabel('frequency [Hz]')
-plt.ylabel('PSD [V**2/Hz]')
-plt.show()
+    per.calc(sample3, rate3)
+
+    plt.plot(per.f, per.Pxx_den)
+    plt.xlabel('frequency [Hz]')
+    plt.ylabel('PSD [V**2/Hz]')
+    plt.show()
+
+
+if __name__ == '__main__':
+    verificar()
 
 
 
