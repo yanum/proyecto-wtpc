1248053afa82d108e3820957c7b6bcf1113b80ec
Carla Gulizia <carla.gulizia@gmail.com>
2016-03-17 15:25:58 -0300

    Agregue calculo de espectro medio de windows - sin testear

--- fFourierTransform.py
+++ fFourierTransform.py
@@ -12,32 +12,40 @@ from scipy.fftpack as fft
 
 class fFourierTransform(object):
 
-    def fFourierTransform(self, windowSample):
-        self.windowSample = windowSample
-        self.windowTimeSample = windowTimeSample
+    def fFourierTransform(self, windowsSample):
+        self.windowsSample = windowsSample
 
 	modtf = []
+        # Arbitrary timestep to creat frequency array for future periodogram graphs
+        timestep = 0.1
 
-        for window in self.windowSample:
+        # This loop goes along each element ("window") in the window sample list
+        for window in self.windowsSample:
+
+            # Calculates for each window the FFT, then the module (real) of the FFT
+            # Append to the new list "modtf" moduloTransfFourier for each window sample.
             tf = fft(window)
             modtf.append( tf * np.conj(tf) )
-	self.moduloTransfFourier = np.array(modtf)
-        self.modulo_Transf_Fourier_Average_de_Windows = 
+            n = len(window) 
+            self.freq = np.fft.fftfreq(n, d=timestep)
+            moduloTransfFourier = np.array(modtf)
+        #Calculates the average periodogram for all the window samples
+        reduce_fft = np.mean(moduloTransfFourier,axis = 0)
 
-        return self.modulo_Transf_Fourier_Average_de_Windows self.moduloTransfFourier
+        #Because the periodogram result is folded (like a mirror), only save n/2 values
+        self.modulo_Transf_Fourier_Average_de_Windows = (reduce_fft [:len(reduce_fft) / 2])
+        return self.modulo_Transf_Fourier_Average_de_Windows
 
     def fft(self): 
 	
-        self.fft = {"time":self.windowTimeSample,"sample":self.sample,"routine": "fFourierTransform")
+        self.fft = {"time":self.freq,"sample":self.modulo_Transf_Fourier_Average_de_Windows,"routine": "fFourierTransform")
         return self.fFT
 
 
 # Test 
 """def test():
-    path = './audioFiles/albanella/'
-    filename = 'call1.wav'
-    fft_out = fFourierTransform()
-    fft_out.fFourierTransform(windowSample,path)
+       fft_out = fFourierTransform()
+       fft_out.fFourierTransform(windowSample)
 
 
 if __name__ == '__main__':
